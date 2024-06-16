c22d2680fd9a67a083d7345311894b45de00f375
diana <dianab_silvaa@hotmail.com>
2024-06-16 19:39:06 -0300

    aprendendo operadores ternários pt.2

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -4,7 +4,7 @@ public class Operadores {
         a = 6;
         b = 6;
         
-        String resultado = a==b ?"verdadeiro" : "falso";
+        int resultado = a==b ? 1 : 0;
         
         System.out.println(resultado);    
 
